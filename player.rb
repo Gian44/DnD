85461d3cd1ce01190dd30b6fe8034b0dc93730f8
Gian44 <gdrenomeron@up.edu.ph>
2023-07-03 23:36:37 +0800

    Added stats

--- player.rb
+++ player.rb
@@ -1,7 +1,8 @@
 class Player
-    attr_accessor :name,:role
-    def initialize (name, role = "Warrior")
+    attr_accessor :name,:role, :stats
+    def initialize (name, role = "Warrior", stats = {Attack: 10, Defense: 10 Health:100, Speed: 10})
         @name = name
         @role = role
+        @stats = stats
     end
 end
